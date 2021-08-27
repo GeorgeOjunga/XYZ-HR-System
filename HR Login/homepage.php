1ddcaed0f1e19821531efdc2846fe334f396de1e
George Ojunga <ojungageorge4@gmail.com>
2021-08-27 13:14:39 +0300

    Add files via upload

---
+++ HR Login/homepage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    
+    <div class="homepage-div">
+        <div class="homepage-header">
+            <div style="float: left; margin-top: 10px; margin-left: 10px;"><img src="images/OJUNGA.JPG.jfif" alt="Company logo " style="width: 80px ; height: 70px;"></div>
+            <div style="float: left; color: white"> <h2>List of Company Employees </h2></div>
+            <div class="topnav">
+                <a href="employee_registration.html">Register</a>
+                <a class="active" href="#news">View</a>
+                
+              </div>
+              
+            <button type="submit" class="btn"  >Manage</button>></button>
+           
+
+        </div>
+        <h2>List of Company Employees </h2>
+
+        
+    </div>
+
+</body>
+</html>
