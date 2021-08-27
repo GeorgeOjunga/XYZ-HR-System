68334dbd3e48f3abfc9a7cf14b46e04d062ddb53
ojunga <ojungageorge4@gmail.com>
2021-08-27 23:36:37 +0300

    Added Procfile

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1748820778df23e08d8de2a30efe1c97d96d6327',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1748820778df23e08d8de2a30efe1c97d96d6327',
+            'dev_requirement' => false,
+        ),
+    ),
+);
